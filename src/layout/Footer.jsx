33f8747c778fc943a0931f35bbd1079d237d5c64
tdizhere <143257261+tdizhere@users.noreply.github.com>
2025-03-09 16:56:18 +0000

    new committed

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -19,21 +19,12 @@ export default function Footer() {
           {/* Built by{" "} */}
           {/* <a href="http://shimon-wosner.vercel.app" target="_blank"> */}
           <a
-            href="https://twitter.com/shwosner"
+            href="#"
             target="_blank"
             rel="noreferrer"
           >
             <FaTwitter style={{ display: "inline" }} />
-            @shwosner
-          </a>
-        </GridItem>
-        <GridItem justifySelf="end">
-          <a
-            href="https://github.com/shwosner/realtime-chat-supabase-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub style={{ display: "inline" }} /> Source code
+            @TanmayDave
           </a>
         </GridItem>
       </Grid>
